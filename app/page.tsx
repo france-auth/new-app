e01eca0fb08b0f75d6edd7ba8140ca806b6468a9
Francis <francis.i.authority@gmail.com>
2025-04-12 20:24:14 +0100

    xd.

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,102 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface TravelItemProps {
+  name: string;
+  numberOfItems: number;
+  packed: boolean;
 }
+
+export default function Home () {
+  const [itemName, setItemName] = useState<string>('');
+  const [itemNumber, setItemNumber] = useState<number>(1);
+  const [isPacked, setIsPacked] = useState<boolean>(false);
+  const [travelItems, setTravelItems] = useState<TravelItemProps[]>([])
+
+  const handleItemName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItemName(e.target.value);
+  }
+  const handleItemNumber = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setItemNumber(Number(e.target.value));
+  }
+  const handleIsPacked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsPacked(e.target.checked);
+  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const newItem: TravelItemProps = {
+      name: itemName,
+      numberOfItems: itemNumber,
+      packed: isPacked
+    }
+
+    setTravelItems((prev) => [...prev, newItem])
+    
+    // Reset Inputs
+    setItemName('');
+    setItemNumber(1)
+    setIsPacked(false)
+  }
+
+  return (
+    <div className="relative flex justify-center items-center min-h-screen w-full">
+      <div className="rounded-sm p-5 w-full flex flex-col max-w-[400px]">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3 items-center w-full bg-blue-600" action="">
+          <h2>Traveling List</h2>
+          <div className="w-full flex flex-col">
+            <label htmlFor="select">
+              How many items?
+            </label>
+            <select 
+              className="text-black" 
+              name="select" 
+              title="select" 
+              onChange={handleItemNumber} 
+              value={itemNumber}
+            >
+              {Array.from({length: 10}, (_, i) => i + 1).map((num) => (
+                <option key={num} value={num}>
+                  {num}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-full flex flex-col">
+            <label htmlFor="items">Item Name</label>
+            <input 
+              value={itemName}
+              onChange={handleItemName}
+              title="items" 
+              type="text" 
+              placeholder="e.g Toothpaste" 
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="isPacked">Is the item packed?</label>
+            <input 
+              checked={isPacked}
+              onChange={handleIsPacked}
+              type="checkbox" 
+              name="isPacked" 
+              id="isPacked" 
+              title="isPacked" 
+            />
+          </div>
+          <button className="w-max px-3 py-2 bg-blue-500" type="submit">Add</button>
+        </form>
+      </div>
+      <div className="mt-6">
+        <h3 className="text-lg font-semibold">🧳 Your Items</h3>
+        <ul className="list-disc list-inside">
+          {travelItems.map((item, index) => (
+            <li key={index}>
+              {item.name} – {item.packed ? '✅ Packed' : '❌ Not Packed'}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
